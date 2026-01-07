e1a644987c7b5dcdbc951689c510b7da7d07ad3f
brad-eck <bradyeckma@gmail.com>
2026-01-06 21:10:35 -0700

    building main file

---
+++ cmd/gossip/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/brad-eck/gossip/internal/config"
+	"github.com/brad-eck/gossip/internal/tui"
+)
+
+func main() {
+	cfg, err := config.load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(cfg.Hosts) == 0 && len(cfg.Groups) == 0 {
+		fmt.Fprintf(os.Stderr, "No hosts defined. Use --hosts or config file.\n")
+		os.Exit(1)
+	}
+
+	p := tea.NewProgram(tui.InitialModel(cfg), tea.WithAltScreen())
+	if err := p.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running gossip: %v\n", err)
+		os.Exit(1)
+	}
+}
