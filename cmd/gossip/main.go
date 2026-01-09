5f392919210b739549143a0cca0a7442291897f5
brad-eck <bradyeckma@gmail.com>
2026-01-09 14:07:13 -0700

    got rid of some nonsense

--- cmd/gossip/main.go
+++ cmd/gossip/main.go
@@ -32,20 +32,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error running gossip: %v\n", err)
 		os.Exit(1)
 	}
-	// cfg, err := config.load()
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
-	// 	os.Exit(1)
-	// }
-
-	// if len(cfg.Hosts) == 0 && len(cfg.Groups) == 0 {
-	// 	fmt.Fprintf(os.Stderr, "No hosts defined. Use --hosts or config file.\n")
-	// 	os.Exit(1)
-	// }
-
-	// p := tea.NewProgram(tui.InitialModel(cfg), tea.WithAltScreen())
-	// if err := p.Start(); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error running gossip: %v\n", err)
-	// 	os.Exit(1)
-	// }
 }
